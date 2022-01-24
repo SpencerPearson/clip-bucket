6b9a6eae25c30806b27d1687c7d16efdbd03d5c2
Spencer Pearson <tigerspence2012@gmail.com>
2022-01-24 11:03:07 -0600

    structured out files for UI layer

---
+++ ClipBucket.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ClipBucket.UI.MVC.Startup))]
+namespace ClipBucket.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
