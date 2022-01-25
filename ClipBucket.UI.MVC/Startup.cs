9908d7844799a47a3062b246e4d21051eb8b9728
Spencer Pearson <tigerspence2012@gmail.com>
2022-01-25 00:32:46 -0600

    Started adding Identity samples

--- ClipBucket.UI.MVC/Startup.cs
+++ ClipBucket.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(ClipBucket.UI.MVC.Startup))]
 namespace ClipBucket.UI.MVC
 {
     public partial class Startup
